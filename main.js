f6ec5b79e5c5bb655f5eb0b229e954aa1c5cde8f
Ajmal Malayil <ajmalmalayil896@gmail.com>
2025-04-18 14:47:28 +0400

    Update main.js

--- main.js
+++ main.js
@@ -343,7 +343,7 @@ window.addEventListener('load', () => {
     
     // Register service worker
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('sw.js')
             .then(registration => {
                 console.log('Service Worker registered with scope:', registration.scope);
             })
